9a8e095f8036d080f312c78160a26fa10264369c
Romen-Adama-Dev <romen_adama.c.r@outlook.com>
2025-12-16 18:45:33 +0000

    Final Chirp

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: 'resources/js/app.js',
-      refresh: true,
-    }),
-    vue(),
-  ],
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
